4b01795fbe879695ae8aa994fa5ec755754ca883
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2022-12-20 17:43:25 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log('JS OK');
+
+// In pieno tema natalizio sfruttiamo le timing functions per  fare il conto alla rovescia per il Natale!
+// Ogni secondo il nostro countdown dovrà scalare fino alla mezzanotte del 25 dicembre!
+
+
+//^ OPERAZIONI PRELIMINARI
+
+// Prendo gli elementi di interesse dalla pagina
+const counterDays = document.getElementById('days');
+const counterHours = document.getElementById('hours');
+const counterMinutes = document.getElementById('minutes');
+const counterSeconds = document.getElementById('seconds');
