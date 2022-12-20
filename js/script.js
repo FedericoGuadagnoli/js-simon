bf63dd172dec0f72d6d448fe2d071af25577567b
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2022-12-20 18:19:40 +0100

    Calcolo la data di fine countdown e quella attuale

--- js/script.js
+++ js/script.js
@@ -4,10 +4,24 @@ console.log('JS OK');
 // Ogni secondo il nostro countdown dovrà scalare fino alla mezzanotte del 25 dicembre!
 
 
-//^ OPERAZIONI PRELIMINARI
 
 // Prendo gli elementi di interesse dalla pagina
 const counterDays = document.getElementById('days');
 const counterHours = document.getElementById('hours');
 const counterMinutes = document.getElementById('minutes');
 const counterSeconds = document.getElementById('seconds');
+
+// Stablisco la data di fine countdown e la determino in millisecondi 
+const deadLine = new Date ("December 25 2022");
+console.log(deadLine);
+
+const deadLineMs = deadLine.getTime();
+console.log(deadLineMs);
+
+// Determino la data attuale in millisecondi
+const nowDateMs = new Date().getTime();
+console.log(nowDateMs);
+
+// Calcolo la differenza 
+const diff = deadLineMs - nowDateMs;
+console.log(diff);
